6cec02e657e344cf237474607dccde50e87b87b2
Aswini <aswiniravichandra99@gmail.com>
2024-09-24 16:00:45 +0530

    changed title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <h1>GEN AI for business and MLOPS</h1>
+    <h1>GEN AI and MLops</h1>
 	<h2>Upload your data to get Gen Ai predictions</h2>
     <img
       alt="Vue logo"
